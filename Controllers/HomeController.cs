75da5c04fd6f27f0182f482b3f839d7d3f39c7a6
NoemiAmori <noemiamorim910@gmail.com>
2022-02-26 21:58:29 -0300

    atualização geral

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,22 +34,23 @@ namespace Hotsite.Controllers
         public IActionResult Cadastrar(Interesse cad)
         {
             DatabaseService dbs = new DatabaseService();
-            dbs.CadastraInteresse(cad);
             try
             {
                 dbs.CadastraInteresse(cad);
             }
             catch (MySqlException m)
             {
-                _logger.LogError("Erro do MySql: " + m.Message);
-                return RedirectToAction("Erro", "Home", m.Message);
+                _logger.LogError(m.Message);
+                return RedirectToAction("Erro");
             }
             catch(Exception e)
             {
                 _logger.LogError("Erro no cadastro: " + e.Message);
-                return RedirectToAction ("Erro", "Home", e.Message);
+                return RedirectToAction ("Erro");
             }
-            return View("Index",cad);
+            ModelState.Clear();
+            ViewData["Mensagem"] = "Cadastro com sucesso";
+            return View ("Index");
         }
 
     }
